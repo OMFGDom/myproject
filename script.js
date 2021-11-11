17d68ca877a38f7403b2ed6e4e0d544c481849ca
Ivan <zazazari434@gmail.com>
2021-11-11 19:28:33 +0600

    second

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberofFilms = +prompt("Сколько фильмов вы уже посмотрели: ", "");
 
 const personalMovieDB = {
@@ -8,15 +10,33 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов: ", ''),
-      b = prompt("На сколько оцените его?: ", ''),
-      c = prompt("Один из последних просмотренных фильмов: ", ''),
-      d = prompt("На сколько оцените его?: ", '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов: ", ''),
+        b = prompt("На сколько оцените его?: ", '');
+    
+    if(a != null && b != null && a != '' && b != '' && a.length <50){
+        personalMovieDB.movies[a] = b;
+        console.log("GOOD");
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}
+
+if (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB => 10 && personalMovieDB < 30 ) {
+    console.log("Вы классический зритель"); 
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("error");
+}
+
 
 console.log(personalMovieDB);
-
     
  

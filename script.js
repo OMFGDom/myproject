58b34bc46ed86604e4007c529224408dab98ae81
Ivan <zazazari434@gmail.com>
2021-11-12 16:39:14 +0600

    third

--- script.js
+++ script.js
@@ -22,29 +22,58 @@ const personalMovieDB = {
 
 
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов: ", ''),
-        b = prompt("На сколько оцените его?: ", '');
-    
-    if(a != null && b != null && a != '' && b != '' && a.length <50){
-        personalMovieDB.movies[a] = b;
-        console.log("GOOD");
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов: ", ''),
+            b = prompt("На сколько оцените его?: ", '');
+        
+        if(a != null && b != null && a != '' && b != '' && a.length <50){
+            personalMovieDB.movies[a] = b;
+            console.log("GOOD");
+        } else {
+            console.log('error');
+            i--;
+        }
+        
     }
-    
 }
 
-if (personalMovieDB.count <= 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB => 10 && personalMovieDB < 30 ) {
-    console.log("Вы классический зритель"); 
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("error");
+rememberMyFilms();
+
+function detectPersonaLevel() {
+    if (personalMovieDB.count <= 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB => 10 && personalMovieDB < 30 ) {
+        console.log("Вы классический зритель"); 
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("error");
+    }
 }
 
+detectPersonaLevel();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB)
+    } else {
+        console.log('fuck ')
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        const e = prompt(`Ваш любимый жанр под номеров ${i}`, '');
+        
+        if(e != null && e != '' && e.length <50){
+            personalMovieDB.genres[i] = e;
+            console.log("GOOD");
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
 console.log(personalMovieDB);
